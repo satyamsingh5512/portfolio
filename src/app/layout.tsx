46fe11e564d0c1c0025a944d27d510cf1322e9f6
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-01-16 17:18:05 +0530

    Add layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
-import UmamiAnalytics from '@/components/analytics/UmamiAnalytics';
-import ChatBubble from '@/components/common/ChatBubble';
-import Footer from '@/components/common/Footer';
-import Navbar from '@/components/common/Navbar';
-import OnekoCat from '@/components/common/OnekoCat';
-import { Quote } from '@/components/common/Quote';
-import { ThemeProvider } from '@/components/common/ThemeProviders';
-import { SessionProvider } from '@/components/providers/SessionProvider';
-import { generateMetadata as getMetadata } from '@/config/Meta';
-import { ViewTransitions } from 'next-view-transitions';
+import UmamiAnalytics from "@/components/analytics/UmamiAnalytics";
+import ChatBubble from "@/components/common/ChatBubble";
+import Footer from "@/components/common/Footer";
+import Navbar from "@/components/common/Navbar";
+import OnekoCat from "@/components/common/OnekoCat";
+import { Quote } from "@/components/common/Quote";
+import { ThemeProvider } from "@/components/common/ThemeProviders";
+import { SessionProvider } from "@/components/providers/SessionProvider";
+import { generateMetadata as getMetadata } from "@/config/Meta";
+import ReactLenis from "lenis/react";
+import { ViewTransitions } from "next-view-transitions";
 
-import './globals.css';
+import "./globals.css";
 
-export const metadata = getMetadata('/');
+export const metadata = getMetadata("/");
 
 export default function RootLayout({
   children,
@@ -29,13 +30,15 @@ export default function RootLayout({
               enableSystem
               disableTransitionOnChange
             >
-              <Navbar />
-              {children}
-              <OnekoCat />
-              <Quote />
-              <Footer />
-              <ChatBubble />
-              <UmamiAnalytics />
+              <ReactLenis root>
+                <Navbar />
+                {children}
+                <OnekoCat />
+                <Quote />
+                <Footer />
+                <ChatBubble />
+                <UmamiAnalytics />
+              </ReactLenis>
             </ThemeProvider>
           </SessionProvider>
         </body>
