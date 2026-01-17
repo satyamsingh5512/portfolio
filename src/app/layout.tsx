58e962198596d3fbddc2b8ef10e079121ed4b08d
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-01-17 23:38:12 +0530

    feat: set dark theme as default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
           <SessionProvider>
             <ThemeProvider
               attribute="class"
-              defaultTheme="system"
+              defaultTheme="dark"
               enableSystem
               disableTransitionOnChange
             >
