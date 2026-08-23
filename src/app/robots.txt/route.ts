c9f08023fb8333ff341b01b1482b9eb914908e5f
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-08-24 00:48:48 +0530

    chore(commit 6): update src/app/robots.txt/route.ts

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -1,6 +1,12 @@
 export function GET() {
   const robots = `User-agent: *
 Allow: /
+Disallow: /admin
+Disallow: /admin/
+Disallow: /api/
+Disallow: /demo
+Disallow: /cp
+Disallow: /maintenance
 
 Sitemap: https://satym.in/sitemap.xml`;
 
