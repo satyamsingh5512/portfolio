42b84e9608bc6d521ed975c71a007de4fc145f40
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-08-24 00:48:58 +0530

    chore(commit 10): update src/app/admin/layout.tsx

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return children;
+}
