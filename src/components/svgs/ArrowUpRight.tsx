23e7db74a5ff631693aa112c20207153b7b492c8
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-08-24 06:28:31 +0530

    chore(batch2 commit 50): update src/components/svgs/ArrowUpRight.tsx

--- src/components/svgs/ArrowUpRight.tsx
+++ src/components/svgs/ArrowUpRight.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export default function ArrowUpRight({ className }: { className?: string }) {
   return (
@@ -8,7 +8,7 @@ export default function ArrowUpRight({ className }: { className?: string }) {
       fill="#000000"
       viewBox="0 0 256 256"
     >
-      <path d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z"></path>
+      <path d="M200 64v104a8 8 0 0 1-16 0V83.31L69.66 197.66a8 8 0 0 1-11.32-11.32L172.69 72H88a8 8 0 0 1 0-16h104a8 8 0 0 1 8 8" />
     </svg>
   );
 }
