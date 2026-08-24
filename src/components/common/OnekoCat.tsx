398c5c773451f04c7abf999c9fa1a7d7290feeb5
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-08-24 06:27:29 +0530

    chore(batch2 commit 29): update src/components/common/OnekoCat.tsx

--- src/components/common/OnekoCat.tsx
+++ src/components/common/OnekoCat.tsx
@@ -1,11 +1,18 @@
-import { catConfig } from '@/config/Cat';
-import Script from 'next/script';
-import React from 'react';
+import { catConfig } from "@/config/Cat";
+import Script from "next/script";
+import React from "react";
 
 export default function OnekoCat() {
   if (!catConfig.enabled) {
     return null;
   }
 
-  return <Script src="./oneko/oneko.js" data-cat="https://res.cloudinary.com/dnuxivxnu/image/upload/v1771768645/portfolio/oneko/file.gif" />;
+  // Absolute path: a relative "./oneko/oneko.js" 404s on any nested route.
+  return (
+    <Script
+      src="/oneko/oneko.js"
+      strategy="lazyOnload"
+      data-cat="https://res.cloudinary.com/dnuxivxnu/image/upload/v1771768645/portfolio/oneko/file.gif"
+    />
+  );
 }
