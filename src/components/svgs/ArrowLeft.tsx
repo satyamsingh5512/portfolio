7c353acb16bd79e640e8e5ff6d12f5b4420ed414
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-08-24 06:28:22 +0530

    chore(batch2 commit 47): update src/components/svgs/ArrowLeft.tsx

--- src/components/svgs/ArrowLeft.tsx
+++ src/components/svgs/ArrowLeft.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export default function ArrowLeft({ className }: { className?: string }) {
   return (
@@ -8,7 +8,7 @@ export default function ArrowLeft({ className }: { className?: string }) {
       viewBox="0 0 256 256"
       className={className}
     >
-      <path d="M232,144a64.07,64.07,0,0,1-64,64H80a8,8,0,0,1,0-16h88a48,48,0,0,0,0-96H51.31l34.35,34.34a8,8,0,0,1-11.32,11.32l-48-48a8,8,0,0,1,0-11.32l48-48A8,8,0,0,1,85.66,45.66L51.31,80H168A64.07,64.07,0,0,1,232,144Z"></path>
+      <path d="M232 144a64.07 64.07 0 0 1-64 64H80a8 8 0 0 1 0-16h88a48 48 0 0 0 0-96H51.31l34.35 34.34a8 8 0 0 1-11.32 11.32l-48-48a8 8 0 0 1 0-11.32l48-48a8 8 0 0 1 11.32 11.32L51.31 80H168a64.07 64.07 0 0 1 64 64" />
     </svg>
   );
 }
