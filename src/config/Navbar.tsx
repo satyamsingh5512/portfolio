ce2606cfaaa4afc73c16bc1ae0880f94e7010e61
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-08-24 06:37:08 +0530

    chore(batch2 commit 106): update src/config/Navbar.tsx

--- src/config/Navbar.tsx
+++ src/config/Navbar.tsx
@@ -5,10 +5,10 @@ export interface NavItem {
 
 export const navbarConfig = {
   logo: {
-    src: "https://res.cloudinary.com/dnuxivxnu/image/upload/v1771769099/portfolio/assets/q0j3puiqnaelv5wp3jhj.jpg",
-    alt: "logo",
-    width: 100,
-    height: 100,
+    src: "/assets/avatar-96.webp",
+    alt: "Satyam",
+    width: 96,
+    height: 96,
   },
   navItems: [
     {
