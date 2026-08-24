da0ba90b8a7f55a83a2f74ae8911b6861733c97c
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-08-24 06:34:16 +0530

    chore(batch2 commit 92): update src/components/technologies/Shadcn.tsx

--- src/components/technologies/Shadcn.tsx
+++ src/components/technologies/Shadcn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export default function Shadcn({ className }: { className?: string }) {
   return (
@@ -9,9 +9,9 @@ export default function Shadcn({ className }: { className?: string }) {
       className={className}
     >
       <path
-        d="m19.01 11.55-7.46 7.46c-.46.46-.46 1.19 0 1.65a1.16 1.16 0 0 0 1.64 0l7.46-7.46c.46-.46.46-1.19 0-1.65s-1.19-.46-1.65 0ZM19.17 3.34c-.46-.46-1.19-.46-1.65 0L3.34 17.52c-.46.46-.46 1.19 0 1.65a1.16 1.16 0 0 0 1.64 0L19.16 4.99c.46-.46.46-1.19 0-1.65Z"
+        d="m19.01 11.55-7.46 7.46a1.16 1.16 0 0 0 0 1.65 1.16 1.16 0 0 0 1.64 0l7.46-7.46c.46-.46.46-1.19 0-1.65s-1.19-.46-1.65 0Zm.16-8.21a1.16 1.16 0 0 0-1.65 0L3.34 17.52a1.16 1.16 0 0 0 0 1.65 1.16 1.16 0 0 0 1.64 0L19.16 4.99c.46-.46.46-1.19 0-1.65Z"
         className="b"
-      ></path>
+      />
     </svg>
   );
 }
