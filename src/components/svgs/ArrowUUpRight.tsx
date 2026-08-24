5ec55f67b67566e29a14cf7d64d917303464200b
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-08-24 06:28:28 +0530

    chore(batch2 commit 49): update src/components/svgs/ArrowUUpRight.tsx

--- src/components/svgs/ArrowUUpRight.tsx
+++ src/components/svgs/ArrowUUpRight.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export default function ArrowUUpRight({ className }: { className?: string }) {
   return (
@@ -8,7 +8,7 @@ export default function ArrowUUpRight({ className }: { className?: string }) {
       viewBox="0 0 256 256"
       className={className}
     >
-      <path d="M170.34,130.34,204.69,96H88a48,48,0,0,0,0,96h88a8,8,0,0,1,0,16H88A64,64,0,0,1,88,80H204.69L170.34,45.66a8,8,0,0,1,11.32-11.32l48,48a8,8,0,0,1,0,11.32l-48,48a8,8,0,0,1-11.32-11.32Z"></path>
+      <path d="M170.34 130.34 204.69 96H88a48 48 0 0 0 0 96h88a8 8 0 0 1 0 16H88a64 64 0 0 1 0-128h116.69l-34.35-34.34a8 8 0 0 1 11.32-11.32l48 48a8 8 0 0 1 0 11.32l-48 48a8 8 0 0 1-11.32-11.32" />
     </svg>
   );
 }
