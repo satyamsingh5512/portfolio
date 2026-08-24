48623cc7bd54a63a13e17e186ab2513dbc1db750
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-08-24 06:27:21 +0530

    chore(batch2 commit 26): update src/components/common/Footer.tsx

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -6,7 +6,7 @@ import VisitorCount from "./VisitorCount";
 
 export default function Footer() {
   return (
-    <Container className="py-10 sm:py-16">
+    <Container as="footer" className="py-10 sm:py-16">
       <div className="flex flex-col items-center justify-center space-y-2">
         <p className="text-secondary text-center text-xs sm:text-sm">
           {footerConfig.text} <b>{footerConfig.developer}</b> <br /> &copy;{" "}
