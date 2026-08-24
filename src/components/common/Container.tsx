9a716717483d3b5bb794bba235f80ce0a7d16762
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-08-24 06:27:18 +0530

    chore(batch2 commit 25): update src/components/common/Container.tsx

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 
+type ContainerElement =
+  | "div"
+  | "section"
+  | "header"
+  | "footer"
+  | "nav"
+  | "main";
+
 export default function Container({
   children,
   className,
+  as: Tag = "div",
   ...props
 }: {
   children: React.ReactNode;
   className?: string;
-}) {
+  as?: ContainerElement;
+} & Omit<React.HTMLAttributes<HTMLElement>, "className" | "children">) {
   return (
-    <div
-      className={`container mx-auto w-full max-w-3xl px-4 sm:px-6 ${className}`}
+    <Tag
+      className={`container mx-auto w-full max-w-3xl px-4 sm:px-6 ${className ?? ""}`}
       {...props}
     >
       {children}
-    </div>
+    </Tag>
   );
 }
